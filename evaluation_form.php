592a121137e9c4ca6d40af7c4fe49d36e947a9a0
bowsse <jaakko.poyhonen@gmail.com>
2017-04-11 23:02:48 +0100

    grade counting

--- evaluation_form.php
+++ evaluation_form.php
@@ -1,6 +1,4 @@
-<?php
-session_start();
-?>
+
 <head>
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet" type="text/css">
@@ -21,40 +19,95 @@ $json = file_get_contents("json/criteria.json");
 //$json = '[{"topic": "1", "subtopic": [{ "1.1 Aiheen vaativuus": ["a"], "1.2": ["b"]}]}]';
 
 $tabledata = json_decode($json, true);
+?>
 
-echo "<section>";
+<section>
 
+<form action="evaluation_form.php" method="post">
+<table border='1'>
+<tr><td></td><td>0</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
 
+<?php
 
-foreach($tabledata as $item)
+foreach($tabledata as $iTopic=>$item)
 {
+	?>
+	<tr><td colspan='7' class="sub">
+	<?php
+	echo "{$item['item']['topic']}</td></tr>\n";
 
-	echo "<table border='1'>\n";
-
-	foreach($item['item']['subtopic'] as $sub)
+	foreach($item['item']['subtopic'] as $iRow=>$sub)
 	{
-		echo '<td>'.$sub['1'][5].'</td>';
+		echo "<tr>";
+		foreach($sub['text'] as $key=>$description)
+		{
+			
+			echo "<td>{$description}";
+
+				if($key > 0)
+			{
+
+				// Radiobutton name and value to match criteria numbering
+				$radioValue = $key - 1;
+				$topic = $iTopic + 1;
+				$row = $iRow + 1;
+				$radioRow = "{$topic}{$row}";
+				echo "<input type='radio' name='" . $radioRow . "' value='" . $radioValue .  "'></td>";
+
+			}
+			else{echo "</td>";}
+		}
+		echo "</tr>";
+
  }
-
-//	echo '<td>'.$item['item']['subtopic'][0]['1'][5].'</td>';
-
-    echo "</table>";
-
-
 }
+echo "</table>\n";
 
-$title1 = $tabledata[0]['item']['subtopic'][0]['1'][0]['text'];
+				echo "<b>Comments</b>"; 
+				$placeholder = "Write your comments here";
 
-$vittu1 = $tabledata[0]['item']['subtopic'][0]['1'][1];
-$vittu2 = $tabledata[0]['item']['subtopic'][0]['1'][2];
-$vittu3 = $tabledata[0]['item']['subtopic'][0]['1'][3];
-$vittu4 = $tabledata[0]['item']['subtopic'][0]['1'][4];
-$vittu5 = $tabledata[0]['item']['subtopic'][0]['1'][5];
-$vittu6 = $tabledata[0]['item']['subtopic'][0]['1'][6];
+?>
+
+<textarea placeholder="<?php echo $placeholder?>"rows="5" cols="50" name="comments" style="width:100%;"></textarea>
+		<br>
+		<div id="button_holder">
+			<button type="submit" value="Submit">Submit</button>
+		</div>
 
 
+</form>
+
+
+<?php
+
+//TODO: Count averages by looping through form
+$grade1 = ($_POST["11"] + $_POST["12"] + $_POST["13"]) / 3;
+$grade2 = ($_POST["21"]) / 1;
+$total = ($grade1 + $grade2) / 2;
+
+echo "<h2> Grade 1: <b> " . $grade1 . "</b> <h2>";
+echo "<h2> Grade 2: <b> " . $grade2 . "</b> <h2>";
+echo "<h1> Total average grade: <b> " . $total . "</b> <h1>";
+
+/*
+$title0 = $tabledata[0]['item']['topic'];
+
+
+$title1 = $tabledata[0]['item']['subtopic'][0]['text'][0]['title'];
+$title2 = $tabledata[0]['item']['subtopic'][1]['text'][0]['title'];
+
+$vittu1 = $tabledata[0]['item']['subtopic'][0]['text'][1];
+$vittu2 = $tabledata[0]['item']['subtopic'][0]['text'][2];
+$vittu3 = $tabledata[0]['item']['subtopic'][0]['text'][3];
+$vittu4 = $tabledata[0]['item']['subtopic'][0]['text'][4];
+$vittu5 = $tabledata[0]['item']['subtopic'][0]['text'][5];
+$vittu6 = $tabledata[0]['item']['subtopic'][0]['text'][6];
+
+
+	echo '<pre>' . print_r($title0,true) . '</pre>';
 
 	echo '<pre>' . print_r($title1,true) . '</pre>';
+		echo '<pre>' . print_r($title2,true) . '</pre>';
 
 	echo '<pre>' . print_r($vittu1,true) . '</pre>';
 	echo '<pre>' . print_r($vittu2,true) . '</pre>';
@@ -64,8 +117,9 @@ $vittu6 = $tabledata[0]['item']['subtopic'][0]['1'][6];
 	echo '<pre>' . print_r($vittu6,true) . '</pre>';
 
 
-
+*/
 echo "</section>";
+/*
 
 echo "<br>";
 echo "<br>";
@@ -76,6 +130,6 @@ echo "<section>";
 	echo '<pre>' . print_r($tabledata, true) . '</pre>';
 
 echo "</section>";
-
+*/
 
 ?>
